92e93583b5152092c7b29e73cbef5b811a36f191
Leonardo Bravo <j.leonardo.bravo@gmail.com>
2026-07-09 15:54:33 -0500

    chore: add tailwind css

--- src/dashboard/vite.config.ts
+++ src/dashboard/vite.config.ts
@@ -1,12 +1,11 @@
 import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
-import { resolve } from 'path';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   root: resolve(__dirname),
   resolve: {
     alias: {
